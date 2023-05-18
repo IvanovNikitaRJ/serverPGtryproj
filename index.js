e20aa9c4b032c24010c31b179d9a4dbe5e6124c9
IvanovNVJ <ivanovnikita11111001110@gmail.com>
2023-05-18 10:57:36 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const userRouter = require('../routes/user.routes');
+const userRouter = require('../routes/user.routes.js');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use('/api', userRouter);
+app.use('/app', userRouter);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
